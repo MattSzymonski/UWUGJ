f5bfc70dbf57d1cb830b8f4be18bb87e7516ab83
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2022-07-09 23:30:00 +0200

    Add UI element chooser.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScoreManager : MonoBehaviour
+public class ScoreManager : MonoBehaviour // TODO: this mixes logic of UI and scoring for now
 {
     public List<int> scoreCaps;
     public int currentCapIdx = 0;
     public int score = 0;
-    List<Element> newChoiceElements;
 
     public GameObject scoreUI;
-    public GameObject inventory;
+    public GameObject[] elements;
+    public GameObject[] buttons; // DO I NEED IT?A
+
+    public Sprite[] spritesForElements;
 
     public Database db;
     // Start is called before the first frame update
     void Start()
-
     {
+
     }
 
     // Update is called once per frame
@@ -35,12 +38,20 @@ public class ScoreManager : MonoBehaviour
 
             ++currentCapIdx;
 
-            newChoiceElements = ReturnNewElements();
+            int idx = 0;
+            foreach (var el in ReturnNewElements())
+            {
+                // lookup the tex for this element
+                elements[idx].GetComponent<Image>().sprite = spritesForElements[(int)el.type];
+                ++idx;
+            }
+
             // rescale the score bar + Juicers
+            
         }
     }
 
-    private List<Element> ReturnNewElements()
+    List<Element> ReturnNewElements()
     {
         List<Element> newElements = new();
         for (int i = 0; i < 4; ++i)
