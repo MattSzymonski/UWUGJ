54cfeadc5a4b026dfecf53f9b87c772f3b961719
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2022-07-10 00:18:24 +0200

    Add Score updating in UI.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour // TODO: this mixes logic of UI and scoring for now
 {
-    public List<int> scoreCaps;
-    public int currentCapIdx = 0;
-    public int score = 0;
+    public int barHeight = 25;
 
-    public GameObject scoreUI;
+    public List<int> scoreTargets;
+    public int currentTargetIdx = 0;
+    public int score = 0;
+    public int lastScore = 0;
+    public int totalScore = 0;
+    
+    public GameObject outerScoreBar;
+    public GameObject innerScoreBar;
+    public GameObject currentScoreText;
+    public GameObject targetScoreText;
     public GameObject[] elements;
     public GameObject[] buttons; // DO I NEED IT?A
 
@@ -19,35 +26,60 @@ public class ScoreManager : MonoBehaviour // TODO: this mixes logic of UI and sc
     // Start is called before the first frame update
     void Start()
     {
-
+        innerScoreBar.GetComponent<RectTransform>().sizeDelta = new Vector2(0, barHeight);
     }
 
     // Update is called once per frame
     void Update()
     {
         // redraw score if changed? in UI
-        // rescale the bar
-        if (score > scoreCaps[currentCapIdx])
+        if (score > lastScore)
         {
-            if (currentCapIdx >= scoreCaps.Capacity)
+            lastScore = score;
+
+            // rescale the bar
+
+            float proportionFilled = score / (float)scoreTargets[currentTargetIdx];
+            // fill by the percentage of current maxSize
+            innerScoreBar.GetComponent<RectTransform>().sizeDelta = 
+                new Vector2(proportionFilled * outerScoreBar.GetComponent<RectTransform>().sizeDelta.x, barHeight);
+
+            currentScoreText.GetComponent<Text>().text = score.ToString();
+
+            // TODO: rescale the score bar + Juicers
+            if (score > scoreTargets[currentTargetIdx])
             {
-                Debug.LogError("Error, add more score ranges!");
-                return;
+                if (currentTargetIdx >= scoreTargets.Capacity)
+                {
+                    Debug.LogError("Error, add more score ranges!");
+                    return;
+                }
+                // TODO: think about triggering map extension and other juicers
+
+                ++currentTargetIdx;
+
+                int idx = 0;
+                foreach (var el in ReturnNewElements())
+                {
+                    // lookup the tex for this element
+                    elements[idx].GetComponent<Image>().sprite = spritesForElements[(int)el.type];
+                    ++idx;
+                }
+
+                // rescale the bar and reset the proportion?
+                totalScore += score;
+                score = 0;
+                lastScore = 0;
+
+                targetScoreText.GetComponent<Text>().text = scoreTargets[currentTargetIdx].ToString();
+                // TODO: smarter rescaling without feeling of going backwards
+                var delta = outerScoreBar.GetComponent<RectTransform>().sizeDelta;
+                outerScoreBar.GetComponent<RectTransform>().sizeDelta =
+                    new Vector2(delta.x + 100, delta.y);
+                proportionFilled = score / (float)scoreTargets[currentTargetIdx];
+                innerScoreBar.GetComponent<RectTransform>().sizeDelta = 
+                    new Vector2(proportionFilled * outerScoreBar.GetComponent<RectTransform>().sizeDelta.x, barHeight);
             }
-            // TODO: think about triggering map extension and other juicers
-
-            ++currentCapIdx;
-
-            int idx = 0;
-            foreach (var el in ReturnNewElements())
-            {
-                // lookup the tex for this element
-                elements[idx].GetComponent<Image>().sprite = spritesForElements[(int)el.type];
-                ++idx;
-            }
-
-            // rescale the score bar + Juicers
-            
         }
     }
 
