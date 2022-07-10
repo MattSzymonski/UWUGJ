890918e6d10c8578ec67e9b263fb74b75d5fdd36
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2022-07-10 07:02:50 +0200

    Add new enums.

--- Assets/Scripts/TypeEnum.cs
+++ Assets/Scripts/TypeEnum.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 
 public enum TypeEnum
 {
-    CORNER_1,
+    CONNECTOR_1,
+    DOME_1,
+    DOME_2,
     MAIN_1,
-    ROOF_1,
-    ROOF_2,
+    MAIN_2,
+    MAIN_SIDE_1,
+    MAIN_SIDE_2,
     SIDE_1,
+    SIDE_2,
     TOWER_1,
     TOWER_2
 }
